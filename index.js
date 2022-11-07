3ec255343e2696ba884aff3198ff4733019f95be
Justin Ong <justin-ong@live.com.au>
2022-11-07 19:54:11 +1100

    added lots

--- index.js
+++ index.js
@@ -58,4 +58,21 @@ function toggleModal() {
   document.body.classList += " modal--open";
 }
 
+let activeIndex = 0;
 
+const groups = document.getElementsByClassName("card-group");
+
+const handleLoveClick = () => {
+  //bump active index
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`);
+  nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+  // Active group becomes afer
+  currentGroup.dataset.status = "after";
+  // Next group becomes active
+  nextGroup.dataset.status = "active";
+  // Update active index next
+  activeIndex = nextIndex;
+  
+};
